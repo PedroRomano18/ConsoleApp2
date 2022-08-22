251e4d73868a25f29ddb42b550c9d49b3d9cb081
Pedro Romano <pedro.romano@hotmail.com>
2022-08-22 16:37:20 -0300

    Iniciando Repositorio

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe seu nome:");
+
+            var nome = Console.ReadLine();
+
+            Console.WriteLine("Infome sua idade");
+
+            var idade = Console.ReadLine();
+
+            int idade2 = Convert.ToInt32(idade);
+            
+         
+            if (idade2 < 18)
+            {
+                Console.WriteLine(idade2 + " anos não bate com os parametros legais! :[");
+                Console.WriteLine(nome + " chame um responsavel legal para entrar no sistema.");
+
+            }
+
+            if (idade2 >= 18)
+            {
+                Console.WriteLine(idade2 + " anos é aceito mos parametros legais");
+                Console.WriteLine(nome + " seja muito bem vindo ao sistema");
+            }
+
+    
+            Console.ReadLine();
+
+            Console.WriteLine("Obrigado professor Samuel! =]");
+
+
+            
+
+            
+
+            
+
+
+
+
+
+
+            Console.ReadKey();
+        
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ ConsoleApp2/Selecao.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("- Digite a nota das 3 atividades:\n");
+
+            decimal nota1, nota2, nota3;
+
+            nota1 = Convert.ToDecimal(Console.ReadLine());
+
+            nota2 = Convert.ToDecimal(Console.ReadLine());
+
+            nota3 = Convert.ToDecimal(Console.ReadLine());
+
+            decimal soma, media;
+
+            soma = (nota1 + nota2 + nota3);
+            media = soma / 3;
+
+            Math.Round(media, 2);
+
+            if (media >= 6)
+            {
+                Console.WriteLine("\n- " + Math.Round(media, 2).ToString() + " APROVADO");
+            }
+            else
+            {
+                Console.WriteLine("\n- " + Math.Round(media, 2).ToString() + " REPROVADO faça a prova de recuperação!");
+
+                decimal nota4;
+
+                nota4 = Convert.ToDecimal(Console.ReadLine());
+
+                soma = (nota1 + nota2 + nota3 + nota4);
+
+                media = soma / 4;
+
+                Math.Round(media, 2);
+
+                if (media >= 6)
+                {
+                    Console.WriteLine("\n- " + Math.Round(media, 2).ToString() + " Parabéns APROVADO!");
+                }
+                else
+                {
+                    Console.WriteLine("\n- " + Math.Round(media, 2).ToString() + " não tem jeito REPROVADO O BURRÃO!");
+                }
+                Math.Round(media, 2);
+            }
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
